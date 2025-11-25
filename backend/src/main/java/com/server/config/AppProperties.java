cff85c968c00369e2024cc700467a8f2eba644ee
Trương Minh Khánh <khanhtmse182131@fpt.edu.vn>
2025-11-25 09:11:05 +0700

    Delete backend directory

--- backend/src/main/java/com/server/config/AppProperties.java
+++
@@ -1,8 +0,0 @@
-package com.server.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "app")
-public class AppProperties {
-    // Add your application properties here
-} 
